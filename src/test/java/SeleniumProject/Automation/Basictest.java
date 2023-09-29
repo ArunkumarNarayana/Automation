bb9697f0bb756e4dd977e86250d3818004325e38
Arunkumar <arunkumar.narayana@gmail.com>
2023-09-29 18:08:31 +0530

    first commit

---
+++ src/test/java/SeleniumProject/Automation/Basictest.java
@@ -0,0 +1,47 @@
+package SeleniumProject.Automation;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.openqa.selenium.AcceptedW3CCapabilityKeys;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+//import com.sun.tools.javac.util.List;
+
+public class Basictest {
+	
+	public static void main(String args[])  {
+	
+	
+		//System.out.println("Hii		");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\ARUN KUMAR\\Downloads\\chromedriver_win32\\chromedriver.exe");  
+        WebDriver driver=new ChromeDriver();  
+        
+        // Launch Website  
+     driver.get("http://www.javatpoint.com/");  
+     driver.manage().window().maximize();
+     //Thread.sleep(3000);
+     @SuppressWarnings("unchecked")
+	List <WebElement> list= driver.findElements(By.xpath("//p[text()='Splunk']/../../../a/div/p"));
+
+     Iterator it=list.iterator();
+     while(it.hasNext())
+     {
+    	 WebElement ele= ((WebElement) it.next());
+    	// driver.navigate().refresh();
+    	 System.out.println(ele.getText());
+    	 
+    	 
+    	 
+     }
+	}
+	
+	
+	
+	}
+	
+
+
---
+++ src/test/java/SeleniumProject/Automation/secondtest.java
@@ -0,0 +1,52 @@
+package SeleniumProject.Automation;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class secondtest {
+	
+	public static void main(String args[]) throws InterruptedException  {
+		
+		
+		//System.out.println("Hii		");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\ARUN KUMAR\\Downloads\\chromedriver_win32\\chromedriver.exe");  
+        WebDriver driver=new ChromeDriver();  
+        
+        // Launch Website  
+     driver.get("http://www.javatpoint.com/");  
+     driver.manage().window().maximize();
+    // driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+     //Thread.sleep(3000);
+     @SuppressWarnings("unchecked")
+	List <WebElement> list= driver.findElements(By.xpath("//ol/li/a"));
+
+     Iterator it=list.iterator();
+     
+    	 
+         for(int i=1;i<list.size();i++) {
+        	 driver.navigate().refresh();
+        	 Thread.sleep(2000);
+        	 System.out.println(driver.findElement(By.xpath("(//ol/li/a)[position()="+i+"]")).getText());
+        	 driver.findElement(By.xpath("(//ol/li/a)[position()="+i+"]")).click();
+        	Thread.sleep(2000);
+        	 driver.navigate().back();
+        	 
+     		
+     	}
+    	 
+    	 
+    	 
+     }
+    
+     
+	}
+	
+	
+
+
