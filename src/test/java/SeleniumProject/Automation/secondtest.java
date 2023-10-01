a06b06722bcf2d14034a59e01a35538fe7d3e96c
ArunkumarNarayana <47077240+ArunkumarNarayana@users.noreply.github.com>
2023-10-01 04:12:53 +0000

    commitin after conflict

--- src/test/java/SeleniumProject/Automation/secondtest.java
+++ src/test/java/SeleniumProject/Automation/secondtest.java
@@ -35,8 +35,7 @@ public class secondtest {
         	 System.out.println(driver.findElement(By.xpath("(//ol/li/a)[position()="+i+"]")).getText());
         	 driver.findElement(By.xpath("(//ol/li/a)[position()="+i+"]")).click();
         	Thread.sleep(2000);
-        	 driver.navigate().back();//modified
-        	 //new row
+        	
         	 
         	 
      		
